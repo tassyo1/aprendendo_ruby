574961c3b5c46bd2d011af857121e2e251bdc866
Tássyo Santana <tassyo.santana@hotmail.com>
2015-12-18 19:18:48 -0200

    18.1 - Singleton

---
+++ singleton.rb
@@ -0,0 +1,27 @@
+class Relatorio
+@@instance = Relatorio.new
+
+def self.instance
+	return @@instance
+end
+
+private_class_method :new
+
+end
+
+relatorio1 =Relatorio.instance
+relatorio2 = Relatorio.instance
+
+#puts relatorio1 == relatorio2
+
+require 'singleton'
+
+class Relatorio2
+include Singleton
+end
+
+rela1 = Relatorio2.instance
+rela2 = Relatorio2.instance
+
+puts rela1 == rela2
+
